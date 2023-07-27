0e7887dbf6c1cd083dd0b11ecf1463ae46f8ccac
lena <ostapets.olena@gmail.com>
2023-07-27 22:51:23 +0300

    tests for Services added

---
+++ src/test/java/com/example/DevTeamProject_Notes/user/UserServiceTest.java
@@ -0,0 +1,36 @@
+package com.example.DevTeamProject_Notes.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class UserServiceTest {
+    @Autowired
+    private UserService userService;
+    @MockBean
+    private UserRepository userRepository;
+    @MockBean
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Test
+    void save() {
+        User user = new User();
+
+        user.setRole(Role.ROLE_USER);
+        user.setPassword("qwerty");
+        when(userRepository.save(user)).thenReturn(user);
+        when(bCryptPasswordEncoder.encode(user.getPassword())).thenReturn("hash");
+        userService.save(user);
+
+        verify(userRepository, times(1)).save(user);
+        assertNotEquals(user.getPassword(), "qwerty");
+        assertEquals(user.getPassword(), "hash");
+    }
+}
