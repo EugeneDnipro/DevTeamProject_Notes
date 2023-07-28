4f6add9a7b0118d95707393634ba289b13dac548
Dmytro Hannych <you@example.com>
2023-07-28 18:56:09 +0300

    correcr file migration and properties

--- src/main/resources/static/migration/h2/V1__create_notes_and_users_tables.sql
+++ src/main/resources/static/migration/h2/V1__create_notes_and_users_tables.sql
@@ -4,15 +4,17 @@ CREATE TABLE users (
   password varchar(100) DEFAULT NULL,
   role varchar(255) DEFAULT NULL,
   PRIMARY KEY (id),
-  UNIQUE KEY UK_sx468g52bpetvlad2j9y0lptc (login)
-)
+  UNIQUE (login)
+);
+
 CREATE TABLE notes (
-  id binary(16) NOT NULL,
+  id UUID NOT NULL,
   content varchar(10000) DEFAULT NULL,
   privacy varchar(255) DEFAULT NULL,
   title varchar(100) DEFAULT NULL,
   user_id bigint DEFAULT NULL,
   PRIMARY KEY (id),
-  KEY FKechaouoa6kus6k1dpix1u91c (user_id),
   CONSTRAINT FKechaouoa6kus6k1dpix1u91c FOREIGN KEY (user_id) REFERENCES users (id)
-)
+);
+CREATE INDEX FKechaouoa6kus6k1dpix1u91c ON notes (user_id);
+
