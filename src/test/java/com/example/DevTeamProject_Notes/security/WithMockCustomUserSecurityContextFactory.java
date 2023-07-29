f96f52d3f153fcf6ae64abb9b19fdf1fc8447a81
lena <ostapets.olena@gmail.com>
2023-07-29 20:20:44 +0300

    note controller tests created

---
+++ src/test/java/com/example/DevTeamProject_Notes/security/WithMockCustomUserSecurityContextFactory.java
@@ -0,0 +1,26 @@
+package com.example.DevTeamProject_Notes.security;
+
+import com.example.DevTeamProject_Notes.user.User;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+
+public class WithMockCustomUserSecurityContextFactory implements WithSecurityContextFactory<WithMockCustomUser> {
+    @Override
+    public SecurityContext createSecurityContext(WithMockCustomUser customUser) {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+
+        User user = new User();
+        user.setId(customUser.id());
+        user.setPassword("qwerty");
+        user.setRole(customUser.role());
+
+        CustomUserDetails principal = new CustomUserDetails(user);
+        Authentication auth = new UsernamePasswordAuthenticationToken(principal, "password", principal.getAuthorities());
+        context.setAuthentication(auth);
+
+        return context;
+    }
+}
